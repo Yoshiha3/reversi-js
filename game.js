8c71385424bb2cd50ce4f7a5a2f70992bf8d45bb
Yoshiha <haru12292997@gmail.com>
2026-06-22 19:03:48 +0900

    feat: コマ設置の仮実装

--- game.js
+++ game.js
@@ -1,10 +1,27 @@
 import Reversi from "./reversi.js";
 import Renderer from "./renderer.js";
+import ClickHandler from "./click-handler.js";
 
 export default class Game {
   constructor() {
     this.reversi = new Reversi();
     this.renderer = new Renderer(this.reversi);
+    this.clickHandler = new ClickHandler(
+      this.renderer.canvas.canvas
+    );
+
+    this.setClickEvent();
+  }
+
+  setClickEvent() {
+    this.clickHandler.onCanvasClick = (coordinates) => {
+      const cellSize = this.renderer.getCellSize();
+      const col = Math.floor(coordinates.x / cellSize);
+      const row = Math.floor(coordinates.y / cellSize);
+
+      this.reversi.setCell(1, col, row);
+      this.render();
+    };
   }
 
   render() {
